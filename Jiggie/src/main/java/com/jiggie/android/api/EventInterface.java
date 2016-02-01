ab270d752e959c4bd0e87b39387056c92bd41d99
aga42 <itu.aga42@gmail.com>
2016-02-01 16:54:46 +0700

    1. Refactor package from com.android.jiggie to com.jiggie.android 2. MVC sample (event)

---
+++ Jiggie/src/main/java/com/jiggie/android/api/EventInterface.java
@@ -0,0 +1,20 @@
+package com.jiggie.android.api;
+
+import com.jiggie.android.model.EventModel;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by LTE on 2/1/2016.
+ */
+public interface EventInterface {
+
+    @GET("http://52.76.76.3/app/v3/events/list/{fb_id}/{gender_interest}")
+    Call<EventModel> getEventList(@Path("fb_id") String fb_id, @Path("gender_interest") String gender_interest);
+
+    /*@GET("http://52.76.76.3/app/v3/events/details/{_id}/{fb_id}/{gender_interest}")
+    Call<EventDetailModel> getEventDetail(@Path("_id") String _id, @Path("fb_id") String fb_id, @Path("gender_interest") String gender_interest);*/
+
+}
---
+++ Jiggie/src/main/java/com/jiggie/android/manager/EventManager.java
@@ -0,0 +1,61 @@
+package com.jiggie.android.manager;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.jiggie.android.api.EventInterface;
+import com.jiggie.android.model.EventModel;
+
+import java.io.IOException;
+
+import de.greenrobot.event.EventBus;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+/**
+ * Created by LTE on 2/1/2016.
+ */
+public class EventManager {
+
+    private static EventInterface eventInterface;
+
+    public static void initEventService(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://52.77.222.216")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        eventInterface = retrofit.create(EventInterface.class);
+    }
+
+    private static void getEventList(String fb_id, String gender_interest, Callback callback) throws IOException {
+        eventInterface.getEventList(fb_id, gender_interest).enqueue(callback);
+    }
+
+    public static void loaderEvent(String fb_id, String gender_interest){
+        try {
+            getEventList(fb_id, gender_interest, new Callback() {
+                @Override
+                public void onResponse(Response response, Retrofit retrofit) {
+
+                    //String header = String.valueOf(response.code());
+
+                    String responses = new Gson().toJson(response.body());
+                    Log.d("res", responses);
+                    EventModel dataTemp = (EventModel) response.body();
+
+                    EventBus.getDefault().post(dataTemp.getData().getEvents());
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    Log.d("Failure", t.toString());
+                }
+            });
+        }catch (IOException e){
+            Log.d("Exception", e.toString());
+        }
+    }
+
+}
