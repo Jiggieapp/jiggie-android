d9c9d681f3c5dac4d8fd59bb684bbb3aba52ad61
aga42 <itu.aga42@gmail.com>
2016-03-01 16:04:34 +0700

    Congrats screen(not finished yet)

---
+++ Jiggie/src/main/java/com/jiggie/android/activity/ecommerce/CongratsActivity.java
@@ -0,0 +1,18 @@
+package com.jiggie.android.activity.ecommerce;
+
+import android.os.Bundle;
+
+import com.jiggie.android.R;
+import com.jiggie.android.component.activity.ToolbarActivity;
+
+/**
+ * Created by LTE on 3/1/2016.
+ */
+public class CongratsActivity extends ToolbarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ticketreserve);
+    }
+}
