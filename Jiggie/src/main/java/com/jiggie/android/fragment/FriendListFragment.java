33629616fd1da8b973761786676a50c399aa3213
wandywijayanto <untukm43N>
2016-05-13 02:35:06 +0700

    Merge branch 'master' of https://github.com/Jiggieapp/jiggie-android
    # Conflicts:
    #	Jiggie/Jiggie.iml

--- Jiggie/src/main/java/com/jiggie/android/fragment/FriendListFragment.java
+++ Jiggie/src/main/java/com/jiggie/android/fragment/FriendListFragment.java
@@ -1,5 +1,6 @@
 package com.jiggie.android.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,18 +8,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jiggie.android.R;
+import com.jiggie.android.activity.chat.ChatActivity;
 import com.jiggie.android.activity.chat.FriendListPresenterImplementation;
 import com.jiggie.android.activity.chat.FriendsFragmentView;
+import com.jiggie.android.component.adapter.ChatTabListAdapter;
 import com.jiggie.android.component.adapter.FriendListAdapter;
+import com.jiggie.android.model.ChatListModel;
+import com.jiggie.android.model.Conversation;
 import com.jiggie.android.model.FriendListModel;
 
 /**
  * Created by Wandy on 5/10/2016.
  */
-public class FriendListFragment extends ChatTabFragment implements FriendsFragmentView {
+public class FriendListFragment extends ChatTabFragment implements FriendsFragmentView,
+        FriendListAdapter.ConversationSelectedListener
+{
 
     private FriendListPresenterImplementation friendListPresenterImplementation;
-    private FriendListAdapter adapter;
+    private FriendListAdapter adapterrr;
+    private static FriendListFragment instance;
+
+    public static FriendListFragment getInstance()
+    {
+        if(instance == null)
+            instance = new FriendListFragment();
+        return instance;
+    }
 
     @Nullable
     @Override
@@ -39,7 +55,7 @@ public class FriendListFragment extends ChatTabFragment implements FriendsFragme
 
     @Override
     public String getTitle() {
-        return "lah";
+        return homeMain.getContext().getString(R.string.friends_title);
     }
 
     @Override
@@ -49,25 +65,35 @@ public class FriendListFragment extends ChatTabFragment implements FriendsFragme
 
     @Override
     public void setAdapter(FriendListModel friendListModel) {
-        adapter.clear();
+        adapterrr.clear();
         for(FriendListModel.Data.List_social_friends list_social_friends : friendListModel.getData().getList_social_friends())
         {
-            adapter.add(list_social_friends);
+            adapterrr.add(list_social_friends);
         }
-        adapter.notifyDataSetChanged();
+        adapterrr.notifyDataSetChanged();
         if(refreshLayout.isRefreshing())
             refreshLayout.setRefreshing(false);
     }
 
     @Override
     protected void setAdapter() {
-        adapter = new FriendListAdapter(super.getContext());
+        adapterrr = new FriendListAdapter(super.getContext(), this);
         recyclerView.setLayoutManager(new LinearLayoutManager(super.getContext()));
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(adapterrr);
     }
 
     @Override
     public void onRefresh() {
         fetchChat();
+    }
+
+
+    @Override
+    public void onConversationSelected(FriendListModel.Data.List_social_friends conversation) {
+        final Intent intent = new Intent(super.getActivity(), ChatActivity.class);
+        intent.putExtra(Conversation.FIELD_PROFILE_IMAGE, conversation.getImg_url());
+        intent.putExtra(Conversation.FIELD_FACEBOOK_ID, conversation.getFb_id());
+        intent.putExtra(Conversation.FIELD_FROM_NAME, conversation.getFirst_name());
+        super.startActivityForResult(intent, 0);
     }
 }
