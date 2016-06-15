98cb140cf19edb8bc051f61caf0b0ebca85d990c
aga42 <itu.aga42@gmail.com>
2016-06-15 11:26:03 +0700

    firebase setup

---
+++ Jiggie/src/main/java/com/jiggie/android/activity/chat/TesFirebaseChatDetailActivity.java
@@ -0,0 +1,16 @@
+package com.jiggie.android.activity.chat;
+
+import android.os.Bundle;
+
+import com.jiggie.android.component.activity.ToolbarActivity;
+
+/**
+ * Created by LTE on 6/14/2016.
+ */
+public class TesFirebaseChatDetailActivity extends ToolbarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
