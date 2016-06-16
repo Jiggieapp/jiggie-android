5efb4a497b28650b326c6ba83adb206b7a489eb3
Wandy Wijayanto <wandywijayanto@Wandys-MacBook-Pro.local>
2016-06-16 17:37:06 +0700

    1226 nambah crashlytics

--- Jiggie/src/main/java/com/jiggie/android/App.java
+++ Jiggie/src/main/java/com/jiggie/android/App.java
@@ -92,13 +92,13 @@ public class App extends Application {
 
         FacebookSdk.sdkInitialize(this);
         AppsFlyerLib.setAppsFlyerKey(super.getString(R.string.appsflyer_devkey));
-        //Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
         //endregion
         this.database = new DatabaseConnection(this);
 
         // Setup handler for uncaught exceptions.
-        //androidDefaultUEH = Thread.getDefaultUncaughtExceptionHandler();
-        //Thread.setDefaultUncaughtExceptionHandler(handler);
+        androidDefaultUEH = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(handler);
 
     }
 
